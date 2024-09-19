e232bc75856bf3e23eaf6effc355b2b197892fa9
Wonderer <zhiyue-chen@ccaschools.org>
2024-09-19 20:29:19 +0000

    Added one enter

--- player.py
+++ player.py
@@ -1,2 +1,3 @@
 class Player:
-    def __init__(self, name, level, prof, sub_prof, stats, hidden_stats, inventory, )
+    def __init__(self, name, level, prof, sub_prof, stats, hidden_stats, inventory):
+        pass
