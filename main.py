508157e7c9ec994878ec71c6cc96b5414c71949d
Wonderer <177084671+aRandomWonder@users.noreply.github.com>
2024-09-27 18:21:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+from time import sleep
+
+
 print("Welcome to Project Zero!")
 print("In this limitless expanse, free from your real world identity, who will you be?")
-print("Please enter the following information:")
+
+while True:
+    choice = input("Do you have an account [y/n] >>   ")
+    if choice.lower == "y":
+        break
+    elif choice.lower == "n":
+        print("Please fill out the questions below.")
+    else:
+        print("Invalid choice. Please try again.")
+
+print("Loading. . .")
+sleep(3)
