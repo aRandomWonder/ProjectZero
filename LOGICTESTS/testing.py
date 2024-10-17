48f3d5b102ac2cd3474079cff561863b7170b307
Wonderer <177084671+aRandomWonder@users.noreply.github.com>
2024-10-17 17:58:29 +0000

    Do more stuff

---
+++ LOGICTESTS/testing.py
@@ -0,0 +1,14 @@
+from time import sleep
+import os
+
+
+def clear_terminal():
+    os.system("clear")
+
+
+# Testing Clear terminal: SUCCESS
+"""
+print("hello")
+sleep(1)
+clear_terminal()
+"""
--- player.py
+++ player.py
@@ -7,7 +7,7 @@ def clear_terminal():
 
 class Player:
     def __init__(self, name = None, level = None, prof = None, sub_prof = None, 
-                 stats = {"vit": None, "str": None, "defe": None, "agi": None, "mana": None}, hidden_stats = {}, 
+                 stats = {"vit": 5, "str": 5, "defe": 5, "agi": 5, "mana": 5}, hidden_stats = {"fortitude": 0, "luck": 0, "dex": 0}, 
                  inventory = {}, equips = {}, status = {}, affil = {}, skills = {}):
         # Basic information
         self.name = name
@@ -48,11 +48,19 @@ class Player:
 
         print("Introduction to the basic 4 stats")
         sleep(0.5)
+
         print("There will be more as time goes on.")
         sleep(0.5)
-        print("Agility - determines your speed.")
+
+        print("Agility - determines your speed. To select, put in \"agil\"")
         sleep(0.5)
-        print("Strength - determines your attack power ")
+
+        print("Defense - determines the damage you recieve. To select, put in \"defe\"")
+        sleep(0.5)
+
+        print("Vitality - determines your health")
+
+        print("Strength - determines your attack power. To select, put in \"str\"")
         input("Press Enter to Move On.")
 
         print("Now you will distribute you're points. You have 15 to freely add.")
@@ -61,9 +69,30 @@ class Player:
 
         clear_terminal()
 
-        for i in range(0, 15):
-            choice = input(f"You have {15-i} points left")
-            input()
+        i = 1
+        while True:
+            choice = input(f"You have {16-i} points left.")
+            choice = choice.lower()
+            if choice == "agil":
+                pass
+            elif choice == "str":
+                pass
+            elif choice == "defe":
+                pass
+            elif choice == "vit":
+                pass
+            else:
+                print("Invalid choice")
+                sleep(0.5)
+                print("Please try again")
+                continue
+            i += 1
+            if i == 15: 
+                print("Selection complete.")
+                sleep(0.5)
+                print("Here are your current attributes: ")
+                input()
+
             
 
 class Skill:
